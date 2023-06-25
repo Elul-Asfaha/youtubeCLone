a95df331f2d22e7b851b3105cc8f331c202bc169
Elul Asfaha <scorpiondota22@gmail.com>
2023-06-25 21:06:25 +0300

    added -videocard component -channelcard component

--- src/utils/FetchFromApi.js
+++ src/utils/FetchFromApi.js
@@ -10,13 +10,6 @@ const options = {
     }
 };
 
-try {
-    const response = await axios.request(options);
-    console.log(response.data);
-} catch (error) {
-    console.error(error);
-}
-
 export const fetchFromApi = async (url) => {
     const { data } = await axios.get(`${BASE_URL}/${url}`, options)
     return data
