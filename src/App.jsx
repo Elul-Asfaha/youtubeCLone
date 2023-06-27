be6dd6f00fdc81b9bfc84fbbbb053f2a3f0adf36
Elul Asfaha <scorpiondota22@gmail.com>
2023-06-27 15:28:55 +0300

    modified the channel display to show data and some visual updates

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
                 <Routes>
                     <Route path='/' exact element={<Feed />} />
                     <Route path='/video/:id' element={<VideoDetail />} />
-                    <Route path='/video/:id' element={<ChannelDetail />} />
+                    <Route path='/channel/:id' element={<ChannelDetail />} />
                     <Route path='/video/:id' element={<SearchFeed />} />
                 </Routes>
             </Box>
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -14,47 +14,61 @@ const VideoCard = ({
         snippet,
     },
 }) => {
-    console.log(snippet);
     return (
-        <Card sx={{ width: { md: "320px", xs: "100%" } }}>
-            <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
-                <CardMedia
-                    image={snippet?.thumbnails?.high?.url}
-                    alt={snippet?.title}
-                    sx={{ width: 358, height: 180, bgcolor: "gray" }}
-                />
-            </Link>
-            <CardContent sx={{ backgroundColor: "#1e1e1e", height: "106px" }}>
-                <Link to={videoId ? `video/${videoId}` : demoVideoUrl}>
-                    <Typography
-                        variant='subtitle1'
-                        fontWeight='bold'
-                        color='#fff'
-                    >
-                        {snippet?.title.slice(0, 60) ||
-                            demoVideoTitle.slice(0, 60)}
-                    </Typography>
+        videoId &&
+        snippet && (
+            <Card
+                sx={{
+                    width: { xs: "100vw", sm: "320px", md: "320px" },
+                    boxShadow: "none",
+                    borderRadius: 0,
+                }}
+            >
+                <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+                    <CardMedia
+                        image={snippet?.thumbnails?.high?.url}
+                        alt={snippet?.title}
+                        sx={{
+                            width: { xs: "100vws", sm: "320px", md: "320px" },
+                            height: 180,
+                            bgcolor: "gray",
+                        }}
+                    />
                 </Link>
-                <Link
-                    to={
-                        snippet?.channelId
-                            ? `channelId/${snippet?.channelId}`
-                            : demoChannelUrl
-                    }
+                <CardContent
+                    sx={{ backgroundColor: "#1e1e1e", height: "106px" }}
                 >
-                    <Typography
-                        variant='subtitle2'
-                        fontWeight='bold'
-                        color='gray'
+                    <Link to={videoId ? `video/${videoId}` : demoVideoUrl}>
+                        <Typography
+                            variant='subtitle1'
+                            fontWeight='bold'
+                            color='#fff'
+                        >
+                            {snippet?.title.slice(0, 60) ||
+                                demoVideoTitle.slice(0, 60)}
+                        </Typography>
+                    </Link>
+                    <Link
+                        to={
+                            snippet?.channelId
+                                ? `channelId/${snippet?.channelId}`
+                                : demoChannelUrl
+                        }
                     >
-                        {snippet?.channelTitle || demoChannelTitle}
-                        <CheckCircle
-                            sx={{ fontSize: 12, color: "gray", ml: "5px" }}
-                        />
-                    </Typography>
-                </Link>
-            </CardContent>
-        </Card>
+                        <Typography
+                            variant='subtitle2'
+                            fontWeight='bold'
+                            color='gray'
+                        >
+                            {snippet?.channelTitle || demoChannelTitle}
+                            <CheckCircle
+                                sx={{ fontSize: 12, color: "gray", ml: "5px" }}
+                            />
+                        </Typography>
+                    </Link>
+                </CardContent>
+            </Card>
+        )
     );
 };
 
